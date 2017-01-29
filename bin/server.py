405c01d19af131d0a4e46e21fa4e0f813a47e855
nh-99 <ifence9@gmail.com>
2017-01-29 12:32:26 -0500

    Add in URL-based clickbait detector (give it a url instead of a headline)

--- bin/server.py
+++ bin/server.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify, request
 from detect import predictor
-
-
+import lxml.html
 
 app = Flask(__name__)
 
+
 @app.route("/detect", methods=["GET"])
-def detect ():
+def detect():
     headline = request.args.get("headline", "")
     clickbaitiness = predictor.predict(headline)
+    return jsonify({ "clickbaitiness": round(clickbaitiness * 100, 2) })
+    
+
+@app.route("/detect/url", methods=["GET"])
+def detect_from_url():
+    url = request.args.get("url", "")
+    clickbaitiness = predictor.predict(lxml.html.parse(url).find(".//title").text)
     return jsonify({ "clickbaitiness": round(clickbaitiness * 100, 2) })
 
 
